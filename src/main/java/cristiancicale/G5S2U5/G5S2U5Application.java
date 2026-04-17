008b012d40cc92f8173d761adecd38a541d09e1e
cicalecristian <cicalecristian@gmail.com>
2026-04-17 10:59:15 +0200

    Initial commit

---
+++ src/main/java/cristiancicale/G5S2U5/G5S2U5Application.java
@@ -0,0 +1,13 @@
+package cristiancicale.G5S2U5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G5S2U5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G5S2U5Application.class, args);
+	}
+
+}
---
+++ src/test/java/cristiancicale/G5S2U5/G5S2U5ApplicationTests.java
@@ -0,0 +1,13 @@
+package cristiancicale.G5S2U5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G5S2U5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
